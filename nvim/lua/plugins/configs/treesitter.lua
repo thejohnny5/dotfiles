f7ac9c49c68af672490f6d236e2d68d9c0d164d5
thejohnny5 <johnny5jg5@gmail.com>
2024-05-19 19:13:42 -0400

    Removed cached nvim

---
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc", "zig", "c" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
